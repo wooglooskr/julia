cf3adaed8df88164d2dcfe5b8c8679599931bc85
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-04-15 17:16:30 -0400

    WIP only use 'true and 'false in ASTs instead of scheme #f and #t
    [ci skip]

--- src/julia-syntax.scm
+++ src/julia-syntax.scm
@@ -282,8 +282,8 @@
             (error "function static parameter names not unique"))
         (if (any (lambda (x) (and (not (eq? x UNUSED)) (memq x names))) anames)
             (error "function argument and static parameter names must be distinct")))
-      (if (and name (not (sym-ref? name)))
-          (error (string "invalid method name \"" (deparse name) "\"")))
+      (if (or (and name (not (sym-ref? name))) (eq? name 'true) (eq? name 'false))
+          (error (string "invalid function name \"" (deparse name) "\"")))
       (let* ((iscall (is-call-name? name))
              (name  (if iscall #f name))
              (types (llist-types argl))
@@ -417,9 +417,11 @@
                  (car not-optional))
             ,@(cdr not-optional) ,@vararg)
           `(block
-            ,@(if (null? lno) '()
-                  ;; TODO jb/functions get a better `name` for functions specified by type
-                  (list (append (car lno) (list (undot-name name)))))
+            ,@(cond ((null? lno) '())
+		    ((not name) lno)
+		    (else
+		     ;; TODO jb/functions get a better `name` for functions specified by type
+		     (list (append (car lno) (list (undot-name name))))))
             ,@stmts) isstaged)
 
         ;; call with unsorted keyword args. this sorts and re-dispatches.
@@ -773,7 +775,8 @@
          ,@(map make-assignment params (symbols->typevars params bounds #t))
          (composite_type ,name (call (top svec) ,@params)
                          (call (top svec) ,@(map (lambda (x) `',x) field-names))
-                         ,super (call (top svec) ,@field-types) ,mut ,min-initialized)))
+                         ,super (call (top svec) ,@field-types) ,(if mut 'true 'false)
+			 ,min-initialized)))
        ;; "inner" constructors
        (scope-block
         (block
@@ -860,7 +863,10 @@
 
 (define (expand-function-def e)   ;; handle function or stagedfunction
   (let ((name (cadr e)))
-    (cond ((and (length= e 2) (symbol? name))  `(method ,name))
+    (cond ((and (length= e 2) (symbol? name))
+	   (if (or (eq? name 'true) (eq? name 'false))
+	       (error (string "invalid function name \"" name "\"")))
+	   `(method ,name))
           ((not (pair? name))                  e)
           ((eq? (car name) 'tuple)
            (expand-forms `(-> ,name ,(caddr e))))
@@ -1085,7 +1091,7 @@
           (let ((err (gensy))
                 (ret (and hasret
                           (or (not (block-returns? tryb))
-                              (and catchb
+                              (and (not (eq? catchb 'false))
                                    (not (block-returns? catchb))))
                           (gensy)))
                 (retval (if hasret (gensy) #f))
@@ -1105,10 +1111,10 @@
                   (break-block
                    ,bb
                    (try (= ,val
-                           ,(if catchb
+                           ,(if (not (eq? catchb 'false))
                                 `(try ,tryb ,var ,catchb)
                                 tryb))
-                        #f
+                        false
                         (= ,err true)))
                   (= ,finally-exception (the_exception))
                   ,finalb
@@ -1123,7 +1129,7 @@
                 (var  (caddr e))
                 (catchb (cadddr e)))
             (expand-forms
-             (if (symbol-like? var)
+             (if (and (symbol-like? var) (not (eq? var 'false)))
                  `(trycatch (scope-block ,tryb)
                             (scope-block
                              (block (= ,var (the_exception))
@@ -2452,7 +2458,7 @@ f(x) = yt(x)
               (composite_type ,name (call (top svec) ,@P)
                               (call (top svec) ,@(map (lambda (v) `',v) fields))
                               ,super
-                              (call (top svec) ,@types) #f ,(length fields))
+                              (call (top svec) ,@types) false ,(length fields))
               (return (null)))))))
 
 ;; ... and without parameters
@@ -2464,7 +2470,7 @@ f(x) = yt(x)
                                   (call (top svec) ,@(map (lambda (v) `',v) fields))
                                   ,super
                                   (call (top svec) ,@(map (lambda (v) 'Any) fields))
-                                  #f ,(length fields))
+                                  false ,(length fields))
                   (return (null))))))
 
 (define (vinfo:not-capt vi)
@@ -3102,10 +3108,10 @@ f(x) = yt(x)
 
             ((method)
              (if (length> e 2)
-                 (begin (emit `(method ,(cadr e)
+                 (begin (emit `(method ,(or (cadr e) 'false)
                                        ,(compile (caddr e) break-labels #t #f)
                                        ,(linearize (cadddr e))
-                                       ,@(cddddr e)))
+                                       ,(if (car (cddddr e)) 'true 'false)))
                         (if value (compile '(null) break-labels value tail)))
                  (cond (tail  (emit-return e))
                        (value e)
